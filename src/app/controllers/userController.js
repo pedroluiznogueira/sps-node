432ca2c855e7a42e9ea98e25f27667b31c85527c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-05 22:00:42 -0300

    validating if email already exists and sending a proper response for each request

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -4,19 +4,20 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const { email } = req.body;
+
     try {
+        if (await User.findOne( {email} ))
+            return res.status(400).send({message: 'email already exists'})
+
         const user = await User.create(req.body); 
-        return res.status(301).send({
-            status: 301,
-            message: 'user created', 
-            user: user
-        }); 
+
+        // don't want to send password back
+        user.password = undefined;
+
+        return res.status(301).send({status: 301, message: 'user created', user: user}); 
     } catch (err) {
-        return res.status(404).send({ 
-            status: 404,
-            message: 'failed to create user',
-            error: err.message
-        });
+        return res.status(404).send({status: 404, message: 'failed to create user', error: err.message});
     }
 });
 
