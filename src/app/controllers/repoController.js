ca18b5df56e715a0d6da6dc8e357b23d0a8f756c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-06 00:48:40 -0300

    adding a repo to a user's repo list

--- src/app/controllers/repoController.js
+++ src/app/controllers/repoController.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const authMiddleware = require('../middlewares/auth');
+const User = require("../models/user");
 
 const router = express.Router();
 router.use(authMiddleware);
 
-router.get('/', async (req, res) => {
-    return res.send({ message: 'middleware working fine'})
+router.post('/add', async (req, res) => {
+    try {
+        const { email, repos } = req.body;
+        
+        const user = await User.findOne({ email });
+        repos.map(repo => user.repos.push(repo));
+        const upd = await User.findByIdAndUpdate(user.id, user);
+
+        res.send({status: 200, message: 'succesfully updated', upd: upd});
+
+    } catch (err) {
+        return res.status(404).send({status: 404, message: 'failed to update', error: err.message});
+    }
 });
 
 module.exports = app => app.use('/repos', router);
