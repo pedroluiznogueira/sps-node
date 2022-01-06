67b3c31d62459c919b96c300d8b234c4963e1ca9
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-05 21:24:32 -0300

    implemented user model, persisting it to mongo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const User = require('./app/models/user');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', async (req, res) => {
-    return res.send({ message: 'api is working fine'});
+app.post('/', async (req, res) => {
+    try {
+        const user = req.body;
+        await User.create(user);
+        return res.send({ message: 'created' });
+    } catch (err) {
+        console.log(err);
+        return res.status(400).send(err);
+    }
 });
 
 app.listen(3000);
