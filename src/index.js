5b992781e0ad57023d0aa08b4d351f0b8f49df41
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-05 21:11:52 -0300

    simple test end-point

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', async (req, res) => {
+    return res.send({ message: 'api is working fine'});
+});
+
 app.listen(3000);
